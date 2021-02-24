884757e8539dadc17fc854bd6faefefaf53a3fd3
linkzepp <adriftchen@gmail.com>
2021-02-24 16:31:25 +0800

    會員

---
+++ front/reg.php
@@ -0,0 +1,48 @@
+<fieldset>
+  <legend>會員註冊</legend>
+  <div style="color:red">請設定您要註冊的帳號及密碼(最常12個字元)</div>
+  <table>
+    <tr>
+      <td>Step1:登入帳號</td>
+      <td><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+      <td>Step2:登入密碼</td>
+      <td><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+      <td>Step3:再次確認密碼</td>
+      <td><input type="text" name="pw2" id="pw2"></td>
+    </tr>
+    <tr>
+      <td>Step4:信箱(忘記密碼時使用)</td>
+      <td><input type="text" name="email" id="email"></td>
+    </tr>
+    <td><input type="submit" value="註冊" onclick="reg()"><input type="reset" value="清除"></td>
+  </table>
+</fieldset>
+
+<script>
+  function reg(){
+    let acc=$("#acc").val()
+    let pw=$("#pw").val()
+    let pw2=$("#pw2").val()
+    let email=$("#email").val()
+
+    if(acc=="" || pw=="" || pw2=="" || email==""){
+      alert("不可空白")
+  }else if(pw=!pw2){
+      alert("密碼錯誤")
+  }else{
+      $.post("api/ckAcc.php",{acc},function(r){
+        if(r=='1'){
+          alert("帳號重複")
+        }else{
+          $.post("api/reg.php",{acc,pw,email},function(){
+            alert("註冊完成，歡迎加入")
+          })
+        }
+      })
+  }
+  }
+</script>
